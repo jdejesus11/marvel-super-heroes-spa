5b256e9d8c732bbe337fd9d39a8f92a916a6c5f4
Juan Carlos De Jesus <j.dejesus@uniandes.edu.co>
2024-01-10 15:29:59 -0500

    Deletes unused images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 import DownArrow from "./images/down-arrow-50.png";
 import RigthArrow from "./images/icons8-right-arrow-50.png";
 import LeftArrow from "./images/icons8-back-50.png";
+import Thrash from "./images/delete-btn.svg";
 
 import Glass from "./images/glass.svg";
 
@@ -9,4 +10,5 @@ export const Illustrations = {
   Glass,
   RigthArrow,
   LeftArrow,
+  Thrash,
 };
